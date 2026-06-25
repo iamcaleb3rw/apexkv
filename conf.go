fb83de29d081fc7353690d271adc17a83fdc2948
iamcaleb3rw <icaleb130@gmail.com>
2026-06-25 17:58:45 +0200

    building a concurrency commands parser

---
+++ conf.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"strconv"
+)
+
+type Config struct {
+	dir string
+	rdb []RDBSnapshot
+	rdbFn string
+	aofEnabled bool
+	aofFn string
+	aofFsync FsyncMode
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
+
+type RDBSnapshot struct {
+	Secs int
+	KeysChanged int
+}
+
+type FsyncMode string
+
+const (
+	Always FsyncMode = "always"
+	EverySec FsyncMode = "everysec"
+	No FsyncMode = "no"
+)
+
+func readConf(fn string) *Config{
+	conf := NewConfig()
+
+	f, err := os.Open(fn)
+	if err != nil {
+		fmt.Printf("cannot read %s - using default config", fn)
+		return conf
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		l := s.Text()
+		parseLine(l, conf)
+	}
+
+	if err := s.Err(); err != nil {
+		fmt.Println("error scanning config file: ", err)
+		return conf
+	}
+
+	if conf.dir != "" {
+		os.MkdirAll(conf.dir, 0755)
+	}
+
+	return conf
+}
+
+func parseLine(l string, conf *Config) {
+	args := strings.Split(l, " ")
+	cmd := args[0]
+	switch cmd {
+	case "save":
+		secs, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("invalid secs")
+			return
+		}
+
+		keysChanged, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println("invalid keys")
+			return 
+		}
+
+		snapshot := RDBSnapshot{
+			Secs: secs, 
+			KeysChanged: keysChanged
+		}
+		conf.rdb = append(conf.rdb, snapshot)
+	case "dbfilename":
+		conf.rdbFn = args[1]
+	case "appendfilename":
+		conf.aofFn = args[1]
+	case "appendfsync":
+		conf.aofFsync = FsyncMode(args[1])
+	case "dir":
+		conf.dir = args[1]
+	case "appendonly":
+		if args[1] == "yes" {
+			conf.aofEnabled = true
+		} else {
+			conf.aofEnabled = false
+		}				
+	}
+
+}
